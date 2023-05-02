a20ee97d6506f84beaedcbddc009962917c4741f
Alireza Hasanzade <alirezawtf321@gmail.com>
2023-05-02 04:42:43 -0700

    chor(thomas): create about page

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("hamburgerMeno").style.display = "block";
+  }
+  
+  function closeNav() {
+    document.getElementById("hamburgerMeno").style.display = "none";
+  }
